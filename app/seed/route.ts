fc35d9a5c7157727f4c862d09137004e489dc0e8
MP Data - Trading <mpiccato@me.com>
2026-06-29 07:08:15 -0300

    bcrypt por bcrypjs

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { db } from '@vercel/postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 import type { VercelPoolClient } from '@vercel/postgres';
